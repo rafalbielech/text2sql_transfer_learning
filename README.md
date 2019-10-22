e162f28f1a5ee81167bd519fb0a88f105f7bd9ae
rafalb15 <rbielech@gmail.com>
2019-10-21 21:32:41 -0400

    Finished datasplitter, updated README

--- README.md
+++ README.md
@@ -10,3 +10,22 @@ python train.py --dataset=../../spider_dataset/ --no_gpu --epochs=100
 ```
 
 Of course, you can use different flags; run ```python train.py --help``` for help w/the flags.
+
+
+## Data Split
+All of our data is found in the {train, train_other, dev}.json files in the original spider dataset. The data split module creates a new folder in this project that contains the files that the modules is expecting in the train and test.
+
+Load_dataset function loads below tables from dataset folder:
+TABLE_PATH = "tables.json"
+TRAIN_PATH = "train.json"
+DEV_PATH = "dev.json"
+TEST_PATH = "dev.json"
+
+Usage: <br>
+from datasplitter import DataSplitter <br><br>
+p = DataSplitter('./spider_dataset') <i>This should be the location of the original spider dataset</i><br><br>
+p.create_dataset_folder("test_dir") <i> Must create new directory before splitting</i><br><br>
+p.merge_data_files("yelp") <br><i>This will merge all json files and then split data so that train.json only contains examples where db_id != yelp and then yelp dataset will be split into the test, dev, and validate files</i><br><br>
+p.delete_dataset_folder() <i>Delete the dataset folder recursively</i><br>
+
+For some reason, DEV_PATH and TEST_PATH right now are expecting a dev.json file thus one is created, however, it is properly split up into test and validate json files using a 0.6 test split. <i>Arbitrarily chosen</i>
--- datasplitter.py
+++ datasplitter.py
@@ -7,23 +7,21 @@ class DataSplitter():
     def __init__(self, path_to_original_dataset):
         self.new_dataset = "" # path to the new dataset that we will be creating
         self.path = path_to_original_dataset # path to the original spider_dataset
-        self.split = 0.7
+        self.split = 0.6
         # check if the path exists, if it does not, raise error
         if (not os.path.exists(self.path)):
             raise ValueError("There is an issue with the path you specified, try again")
 
     def return_path(self):
-        # just return path
-        return self.path
+        return self.path # just return path
 
     def return_directory(self):
-        # return the directory of the object's spider specified path
-        return os.path.dirname(self.path)
+        return os.path.dirname(self.path) # return the directory of the object's spider specified path
 
     def create_dataset_folder(self, name_of_new_dir):
         # create a new directory in the same directory as the object's specified spider dataset
-        curr_dir = self.return_directory()
-        curr_dir = os.path.join(curr_dir, name_of_new_dir)
+        curr_dir = os.path.join(self.return_directory(), name_of_new_dir)
+        # check if the directory already exists, if not, then print message
         if not os.path.exists(curr_dir):
             try:
                 os.mkdir(curr_dir)
@@ -35,6 +33,7 @@ class DataSplitter():
             print("Directory " , name_of_new_dir ,  " already exists, operation terminated")
 
     def delete_dataset_folder(self):
+        # first check if the path exists or if self.new_dataset is set, if not, then only print to screen
         if os.path.exists(self.new_dataset):
             try:
                 shutil.rmtree(os.path.abspath(self.new_dataset))
@@ -45,9 +44,13 @@ class DataSplitter():
         else:
             print("Nothing to delete, the path does not exist or self.new_dataset is empty")
 
-    def merge_data_files(self, file_names=["train", "train_others", "dev"]):
+    def merge_data_files(self, database, file_names=["train", "train_others", "dev"]):
+        ######### filenames ###########################
         # file names contains all of our examples across three different json files
         # we will be merging all of the three files together to then create our data
+        ######### database ###########################
+        # the name of the database that we want to split out list of data on
+        ##############################################
         def filter_out_duplicates(list_of_elements):
             # Use set to filter out duplicates SQL examples from the merged files
             print("Before, length of list was {}".format(len(list_of_elements)))
@@ -62,6 +65,43 @@ class DataSplitter():
                     new_list.append(d)
             print("After, length of list was {}".format(len(new_list)))
             return new_list
+
+        def move_file_to_new_dir(file):
+            try:
+                file = os.path.join(self.path, file + ".json")
+                shutil.copy(file, self.new_dataset)
+            except Exception as e:
+                print("Error moving file {}\nError: {}".format(file, e))
+
+        def split_based_on_database(designated_database, combined_list):
+            def write_to_json(file_name, list):
+                temp_file_path = os.path.join(self.new_dataset, file_name + ".json")
+                try:
+                    json.dump(list, open(temp_file_path , "w"))
+                except Exception as e:
+                    print("Error writing {}\nError: {}".format(temp_file_path, e))
+
+
+            def split_test_val(list_to_split):
+                test = list_to_split[:int(len(list_to_split) * self.split)]
+                val = list_to_split[int(len(list_to_split) * self.split):]
+                return test, val
+                # databases contains the list of databases that are in our combined list of unique entries
+            databases = [item['db_id'] for item in combined_list]
+
+            if designated_database in databases:
+                print("{} is in the list".format(designated_database))
+                all_except_designated = [item for item in combined_list if item['db_id'] != designated_database]
+                designated = [item for item in combined_list if item['db_id'] == designated_database]
+                write_to_json("train", all_except_designated)
+                write_to_json("dev", designated)
+                test, val = split_test_val(designated)
+                write_to_json("test", test)
+                write_to_json("validate", val)
+            else:
+                print("{} is in not the list".format(designated_database))
+                write_to_json("all_train", combined_list)
+
         # only proceed if there is a new dataset folder already created
         if (self.new_dataset != ""):
             combined_file = []
@@ -71,9 +111,12 @@ class DataSplitter():
                     for entry in json.load(in_file):
                         combined_file.append(entry)
             try:
+                # model expects a tables.json file
+                move_file_to_new_dir("tables")
+                # remove duplicates
                 combined_file = filter_out_duplicates(combined_file)
-                temp_file_path = os.path.join(self.new_dataset, "train.json")
-                json.dump(combined_file, open(temp_file_path , "w"))
+                # check if the designed database exists in the combined file list, if it does, then
+                split_based_on_database(database, combined_file)
             except Exception as e:
                 print("Error merging file and saving to new directory\nError: {}".format(e))
         else:
