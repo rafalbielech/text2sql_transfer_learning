9d0af55699579ca8aa537ddccb90c4f82c9f9eaf
agr1997 <g.arvind1@gmail.com>
2019-11-08 20:23:54 -0500

    Update retrain.py

--- models/sqlnet/retrain.py
+++ models/sqlnet/retrain.py
@@ -5,6 +5,7 @@ import argparse
 import numpy as np
 from scripts.utils import *
 from scripts.model.sqlnet import SQLNet
+import os
 
 
 if __name__ == '__main__':
@@ -13,18 +14,25 @@ if __name__ == '__main__':
             help='If set, use small data; used for fast debugging.')
     parser.add_argument('--dataset', type=str, default='',
             help='to dataset directory where includes train, test and table json file.')
-    parser.add_argument('--saved_models', type=str,
-            help='path to the saved train models')
+    parser.add_argument('--base_model_path', type=str,
+            help='path to the saved trained models')
+    parser.add_argument('--save_to_path', type=str,
+            help='path to save the retrained models')
     parser.add_argument('--train_emb', action='store_true',
             help='Train word embedding.')
     parser.add_argument('--no_gpu', action='store_true',
             help='If set, dont use the GPU')
     parser.add_argument('--epochs', type=int, default=100,
-            help='the number of epochs to train on')
+            help='the number of epochs to retrain on')
     parser.add_argument('--learning_rate', type=float, default=1e-3,
             help='learning rate based on Adam update')
     parser.add_argument('--weight_decay', type=float, default=0,
             help='decay rate of weights for Adam update')
+    # TODO: Add arguement to accept number of layers to retrain on.
+    # Leads to additional guesswork.
+    # TODO: Find out shapes and types of layers in the model
+    # Might only need to focus on the FC layer. 
+    # Sizes of the smallest and largest dataset in SPIDER: 4, 569
 
 
     args = parser.parse_args()
@@ -52,6 +60,16 @@ if __name__ == '__main__':
 
     model = SQLNet(word_emb, N_word=N_word, gpu=GPU, trainable_emb=args.train_emb)
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay = 0)
+    
+    print("Loading from sel model...")
+    model.sel_pred.load_state_dict(torch.load(os.path.join(args.base_model_path, "/sel_models.dump")))
+    print("Loading from sel model...")
+    model.cond_pred.load_state_dict(torch.load(os.path.join(args.base_model_path, "/cond_models.dump")))
+    print("Loading from sel model...")
+    model.group_pred.load_state_dict(torch.load(os.path.join(args.base_model_path, "/group_models.dump")))
+    print("Loading from sel model...")
+    model.order_pred.load_state_dict(torch.load(os.path.join(args.base_model_path, "/order_models.dump")))
+
 
     #initial accuracy
     init_acc = epoch_acc(model, BATCH_SIZE, val_sql_data, val_table_data, schemas, TRAIN_ENTRY)
@@ -70,26 +88,30 @@ if __name__ == '__main__':
         print(' Breakdown results: sel: %s, cond: %s, group: %s, order: %s'\
             % (train_bkd_acc[0], train_bkd_acc[1], train_bkd_acc[2], train_bkd_acc[3]))
 
-
+        val_tot_acc, val_bkd_acc = epoch_acc(model, BATCH_SIZE, val_sql_data, val_table_data, schemas, TRAIN_ENTRY, error_print = False, train_flag = False) #for detailed error analysis, pass True to error_print
+        print(' Dev acc_qm: %s' % val_tot_acc)
+        print(' Breakdown results: sel: %s, cond: %s, group: %s, order: %s'\
+            % (val_bkd_acc[0], val_bkd_acc[1], val_bkd_acc[2], val_bkd_acc[3]))
+        
         #save models
-        if train_bkd_acc[0] > best_sel_acc:
+        if val_bkd_acc[0] > best_sel_acc:
             best_sel_acc = train_bkd_acc[0]
             print("Saving sel model...")
-            torch.save(model.sel_pred.state_dict(), "retrain_saved_models/sel_models.dump")
-        if train_bkd_acc[1] > best_cond_acc:
+            torch.save(model.sel_pred.state_dict(), os.path.join(args.save_to_path, "/sel_models.dump"))
+        if val_bkd_acc[1] > best_cond_acc:
             best_cond_acc = train_bkd_acc[1]
             print("Saving cond model...")
-            torch.save(model.cond_pred.state_dict(), "retrain_saved_models/cond_models.dump")
-        if train_bkd_acc[2] > best_group_acc:
+            torch.save(model.cond_pred.state_dict(), os.path.join(args.save_to_path, "/cond_models.dump"))
+        if val_bkd_acc[2] > best_group_acc:
             best_group_acc = train_bkd_acc[2]
             print("Saving group model...")
-            torch.save(model.group_pred.state_dict(), "retrain_saved_models/group_models.dump")
+            torch.save(model.group_pred.state_dict(), os.path.join(args.save_to_path, "/group_models.dump"))
         if train_bkd_acc[3] > best_order_acc:
             best_order_acc = train_bkd_acc[3]
             print("Saving order model...")
-            torch.save(model.order_pred.state_dict(), "retrain_saved_models/order_models.dump")
+            torch.save(model.order_pred.state_dict(), os.path.join(args.save_to_path, "/order_models.dump"))
         if val_tot_acc > best_tot_acc:
             best_tot_acc = val_tot_acc
 
 
-        print(' Best val sel = %s, cond = %s, group = %s, order = %s, tot = %s'%(best_sel_acc, best_cond_acc, best_group_acc, best_order_acc, best_tot_acc))
+print(' Best val sel = %s, cond = %s, group = %s, order = %s, tot = %s'%(best_sel_acc, best_cond_acc, best_group_acc, best_order_acc, best_tot_acc))
