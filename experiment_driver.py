f5e56342881c96d4c33c3bc3e501bb378314b93a
Pavitra Dangati <pavitradangati@Pavitras-MacBook-Air.local>
2019-11-16 16:50:11 -0500

    Adding experiment driver and file handling

---
+++ experiment_driver.py
@@ -0,0 +1,46 @@
+import subprocess
+import argparse
+import json
+
+def process_arguments(arguments, i):
+    kwargs=[]
+    for key in arguments:
+            if key in ["new_dataset_dir", "dataset", "save_to_path", "base_model_path"]:
+                kwargs.append("--"+key+"="+arguments[key]+str(i))
+            elif key in ["database_num"]:
+                kwargs.append("--"+key+"="+str(i))
+            elif type(arguments[key])==bool:
+                if arguments[key]:
+                    kwargs.append("--"+key) 
+            else:
+                kwargs.append("--"+key+"="+str(arguments[key]))
+    return kwargs
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--start', type=int, default=1,
+            help='Starting range for databases to train, retrain')
+    parser.add_argument('--end', type=int, default=5,
+            help='Ending range for databases to train, retrain')
+    args = parser.parse_args()
+
+    #unpack
+    with open('config.json') as json_data_file:
+        config = json.load(json_data_file)
+    subprocess.call(config["unpack"]["execCommand"])
+
+    for i in range(args.start, args.end+1):
+        print('Starting datasplitting for', i)
+        #datasplitter
+        kwargs=process_arguments(config["datasplitter"]["arguments"], i)
+        subprocess.call(config["datasplitter"]["execCommand"]+kwargs)
+    
+        print('Calling train for', i) 
+        kwargs_train = process_arguments(config["train"]["arguments"], i)
+        subprocess.call(config["train"]["execCommand"]+kwargs_train)
+        
+        print('Calling retrain for', i)
+        kwargs_retrain = process_arguments(config["retrain"]["arguments"], i)
+        subprocess.call(config["retrain"]["execCommand"]+kwargs_retrain)
+        break
--- models/sqlnet/train.py
+++ models/sqlnet/train.py
@@ -12,6 +12,8 @@ if __name__ == '__main__':
             help='If set, use small data; used for fast debugging.')
     parser.add_argument('--dataset', type=str, default='',
             help='to dataset directory where includes train, test and table json file.')
+    parser.add_argument('--save_to_path', type=str, default='',
+            help='path to the saved trained models')
     parser.add_argument('--train_emb', action='store_true',
             help='Train word embedding.')
     parser.add_argument('--no_gpu', action='store_true',
@@ -20,7 +22,7 @@ if __name__ == '__main__':
             help='the number of epochs to train on')
 
     args = parser.parse_args()
-
+    print(args)
     N_word=50
     B_word=6
     GPU = not args.no_gpu
@@ -39,51 +41,55 @@ if __name__ == '__main__':
             TRAIN_DB, DEV_DB, TEST_DB = load_dataset(args.dataset, use_small=USE_SMALL)
 
 
-    word_emb = load_word_emb('glove/glove.%dB.%dd.txt'%(B_word,N_word), \
+    word_emb = load_word_emb('./models/sqlnet/glove/glove.%dB.%dd.txt'%(B_word,N_word), \
             load_used=args.train_emb, use_small=USE_SMALL)
+
+    #Check if directory exists. Else create one!
+    if not os.path.isdir(args.save_to_path):
+        os.mkdir(args.save_to_path)
+
+   
 
     model = SQLNet(word_emb, N_word=N_word, gpu=GPU, trainable_emb=args.train_emb)
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate, weight_decay = 0)
-
     #initial accuracy
     init_acc = epoch_acc(model, BATCH_SIZE, val_sql_data, val_table_data, schemas, TRAIN_ENTRY)
-
+    print('Initial acc...', init_acc)
     best_sel_acc = init_acc[1][0]
     best_cond_acc = init_acc[1][1]
     best_group_acc = init_acc[1][2]
     best_order_acc = init_acc[1][3]
     best_tot_acc = 0.0
-
     for i in range(args.epochs):
         print('Epoch %d @ %s'%(i+1, datetime.datetime.now()))
         print(' Loss = %s'%epoch_train(model, optimizer, BATCH_SIZE, sql_data, table_data, schemas, TRAIN_ENTRY))
         train_tot_acc, train_bkd_acc = epoch_acc(model, BATCH_SIZE, sql_data, table_data, schemas, TRAIN_ENTRY, train_flag = True)
         print(' Train acc_qm: %s' % train_tot_acc)
         print(' Breakdown results: sel: %s, cond: %s, group: %s, order: %s'\
-            % (train_bkd_acc[0], train_bkd_acc[1], train_bkd_acc[2], train_bkd_acc[3]))
-
+            % (train_bkd_acc[0], train_bkd_acc[1], train_bkd_acc[2], train_bkd_acc[3])) 
         val_tot_acc, val_bkd_acc = epoch_acc(model, BATCH_SIZE, val_sql_data, val_table_data, schemas, TRAIN_ENTRY, error_print = False, train_flag = False) #for detailed error analysis, pass True to error_print
         print(' Dev acc_qm: %s' % val_tot_acc)
         print(' Breakdown results: sel: %s, cond: %s, group: %s, order: %s'\
             % (val_bkd_acc[0], val_bkd_acc[1], val_bkd_acc[2], val_bkd_acc[3]))
 
+        
         #save models
         if val_bkd_acc[0] > best_sel_acc:
             best_sel_acc = val_bkd_acc[0]
             print("Saving sel model...")
-            torch.save(model.sel_pred.state_dict(), "saved_models/sel_models.dump")
+            torch.save(model.sel_pred.state_dict(), args.save_to_path+"/sel_models.dump")
         if val_bkd_acc[1] > best_cond_acc:
             best_cond_acc = val_bkd_acc[1]
             print("Saving cond model...")
-            torch.save(model.cond_pred.state_dict(), "saved_models/cond_models.dump")
+            torch.save(model.cond_pred.state_dict(), args.save_to_path+"/cond_models.dump")
         if val_bkd_acc[2] > best_group_acc:
             best_group_acc = val_bkd_acc[2]
             print("Saving group model...")
-            torch.save(model.group_pred.state_dict(), "saved_models/group_models.dump")
+            torch.save(model.group_pred.state_dict(), args.save_to_path+"/group_models.dump")
         if val_bkd_acc[3] > best_order_acc:
             best_order_acc = val_bkd_acc[3]
             print("Saving order model...")
-            torch.save(model.order_pred.state_dict(), "saved_models/order_models.dump")
+            torch.save(model.order_pred.state_dict(), args.save_to_path+"/order_models.dump")
         if val_tot_acc > best_tot_acc:
             best_tot_acc = val_tot_acc
 
